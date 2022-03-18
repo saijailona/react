496b8debcf06f20c6b4a810b2394a9745ff63f5b
saijailona <sailotem@gmail.com>
2022-03-18 14:05:22 +0200

    task a

--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -1,7 +1,30 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import MediaRow from './MediaRow';
+import {useEffect, useState} from 'react';
 
-const MediaTable = ({mediaArray}) => {
+
+
+
+const MediaTable = () => {
+
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const getMedia = async () => {
+    try {
+   const response = await fetch('test.json');
+   const json = await response.json();
+   setMediaArray(json);
+    } catch (err) {
+      console.error(err.message);
+    }
+
+  };
+
+  useEffect( () => {
+    getMedia();
+  }, []);
+
+
   console.log(mediaArray);
   return (
     <table>
@@ -14,7 +37,5 @@ const MediaTable = ({mediaArray}) => {
   );
 };
 
-MediaTable.propTypes = {
-  mediaArray: PropTypes.array.isRequired,
-};
+
 export default MediaTable;
